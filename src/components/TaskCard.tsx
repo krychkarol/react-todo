7c1ce897643179b2f1bd65c270ea73299a82bfd9
krychkarol <krych.karol.93@gmail.com>
2023-02-27 17:25:00 +0100

    add, edit, delete

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Task } from '../model';
 import './styles.css';
 import { RiEdit2Line, RiDeleteBin2Line, RiCheckLine } from 'react-icons/ri'
@@ -10,23 +10,55 @@ interface Props {
 }
 
 const TaskCard: React.FC<Props> = ({ task, todos, setTodos }) => {
+
+    const [isEdit, setIsEdit] = useState<boolean>(false)
+    const [taskText, setTaskText] = useState<string>(task.task)
+
+    const handleIsDone = (id: number) => {
+        setTodos(todos.map(task => task.id === id ? { ...task, isDone: !task.isDone } : task))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter(task => task.id !== id))
+    }
+
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTodos(todos.map(task => task.id === id ? { ...task, task: taskText } : task))
+        setIsEdit(false)
+    }
+
+    const editTaskRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        editTaskRef.current?.focus()
+    }, [isEdit])
+    
+
     return (
-        <form className='task_form'>
+        <form className='task_form' onSubmit={(e) => handleEdit(e, task.id)}>
             <div className="task_form_wrapper">
-                <div className='task'>{task.task}</div>
+                {
+                    isEdit ?
+                        <input className='task_edit' ref={editTaskRef} value={taskText} onChange={(e) => setTaskText(e.target.value)} /> :
+                        task.isDone ?
+                            <div className='task task_done'>{task.task}</div> :
+                            <div className='task'>{task.task}</div>
+                }
+
                 <div className='icon_box'>
-                    <span className='icon'>
+                    <span className='icon' onClick={(e) => { !isEdit ? setIsEdit(!isEdit) : handleEdit(e, task.id) }}>
                         <RiEdit2Line />
                     </span>
-                    <span className='icon'>
+                    <span className='icon' onClick={() => handleDelete(task.id)}>
                         <RiDeleteBin2Line />
                     </span>
-                    <span className='icon'>
+                    <span className='icon' onClick={() => handleIsDone(task.id)}>
                         <RiCheckLine />
                     </span>
                 </div>
             </div>
-            
+
         </form>
     )
 }
